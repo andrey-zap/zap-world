27a4427c806637b89117aa4889ad6265956a6af2
andrey-zap <zaplavnov@gmail.com>
2020-04-25 15:36:06 +0300

    gitignore modification

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hey, we have Flask in a Docker container!"
+    return "Hey, we have Flask in a Docker container with 100 mb size!!"
     
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0',port='5001')
